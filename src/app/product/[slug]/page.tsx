14205dbae88f5d651292f33c4a510706649e09e9
Hammad Faisal <145413000+hammad910@users.noreply.github.com>
2025-01-23 12:26:13 +0500

    correction

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -3,11 +3,6 @@ import RelatedProduct from '@/components/product/realedProduct';
 import { getProductBySlug } from '@/sanity/lib/queries';
 import React from 'react';
 
-// Correct the params type for Next.js
-interface Params {
-  slug: string;
-}
-
 // Use the appropriate PageProps structure
 const page = async ({ params }: { params: { slug: string } }) => {
   const { slug } = params;
